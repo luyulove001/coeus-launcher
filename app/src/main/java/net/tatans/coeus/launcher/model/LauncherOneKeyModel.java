f0865faee063b0d88e7a3d74661018262846be9f
SiLiping <939753553@qq.com>
2016-02-19 10:21:21 +0800

    央广新闻（酷fm）打开出现停止运行，删除一键央广新闻

--- app/src/main/java/net/tatans/coeus/launcher/model/LauncherOneKeyModel.java
+++ app/src/main/java/net/tatans/coeus/launcher/model/LauncherOneKeyModel.java
@@ -24,8 +24,8 @@ public class LauncherOneKeyModel implements ILauncherOneKeyModel {
 		List<LauncherOneKeyBean> al_launcher= tdb.findAll(LauncherOneKeyBean.class);
 		if(al_launcher.size()<6){
 			LauncherOneKeyBean launcherBean = new LauncherOneKeyBean();
-			String[] names = {"新闻", "电台", "随心听","笑话", "本地音乐", "天气","央广新闻",OneKeyFengHuangFMMusic.oneKeyName};
-			String[] sDes = {"新闻", "电台", "播放在线音乐", "笑话", "播放本地音乐","天气","央广新闻",OneKeyFengHuangFMMusic.oneKeyName};
+			String[] names = {"新闻", "电台", "随心听","笑话", "本地音乐", "天气",OneKeyFengHuangFMMusic.oneKeyName};
+			String[] sDes = {"新闻", "电台", "播放在线音乐", "笑话", "播放本地音乐","天气",OneKeyFengHuangFMMusic.oneKeyName};
 			int[] arr_nID = {
 					0,
 					1,
@@ -33,7 +33,6 @@ public class LauncherOneKeyModel implements ILauncherOneKeyModel {
 					3,
 					4,
 					5,
-					6,
 					9
 			};
 //			tdb.delete(LauncherOneKeyBean.class);
